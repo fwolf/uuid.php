6796fe715541bcf3d001bab01bf96e5fc741dd50
Fwolf <fwolf.aide+git@gmail.com>
2016-09-21 16:34:21 +0800

    Create PHPUnit configure

---
+++ bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Bootstrap
+ *
+ *  - Register ClassLoader
+ *  - Load default and user config
+ *
+ * @copyright   Copyright 2016 Fwolf
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
+
+// Include autoloader
+$classLoader = require __DIR__ . '/vendor/autoload.php';
